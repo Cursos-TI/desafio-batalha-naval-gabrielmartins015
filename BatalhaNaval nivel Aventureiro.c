5194dedfeac687b386f4b9640123bbe7b23ff5a8
gabrielmartins015 <gabrielghmpy@gmail.com>
2025-11-13 06:02:32 +0000

    BatalhaNaval Nivel Aventureiro

---
+++ BatalhaNaval nivel Aventureiro.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+   int main() {
+    
+    char colunaLetra [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+      
+    int tabuleiro[10][10];
+     
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    
+    int linhaHorizontal = 1;  
+    int colunaHorizontal = 3; 
+    int linhaVertical = 4;    
+    int colunaVertical = 6;   
+    int linhaDiagonal1 = 1;
+    int colunaDiagonal1 = 1;
+    int linhaDiagonal2 = 7;
+    int colunaDiagonal2 = 9;
+    
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaHorizontal][colunaHorizontal + i] = 3;
+    }
+
+    
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaVertical + i][colunaVertical] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaDiagonal2 + i][colunaDiagonal2 - i] = 3;
+    }
+
+    printf("*** COORDENADAS DOS NAVIOS ***\n");
+    printf("Navio horizontal:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("(%c, %d)\n", colunaLetra[colunaHorizontal + i], linhaHorizontal + 1);
+    }
+
+    printf("Navio vertical:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("(%c, %d)\n", colunaLetra[colunaVertical], linhaVertical + i + 1);
+    }
+    
+    printf("Navio diagonal ↘:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("(%c, %d)\n", colunaLetra[colunaDiagonal1 + i], linhaDiagonal1 + i + 1);
+    }
+
+    printf("Navio diagonal ↙:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("(%c, %d)\n", colunaLetra[colunaDiagonal2 - i], linhaDiagonal2 + i + 1);
+    }
+
+
+
+
+    printf("\n*** TABULEIRO BATALHA NAVAL ***\n\n");
+
+    
+    printf("    ");
+    for (int c = 0; c < 10; c++) {
+        printf("%c ", colunaLetra[c]);
+    }
+    printf("\n");
+
+   
+    for (int i = 0; i < 10; i++) {
+        printf("%2d: ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("    ");
+    for (int c = 0; c < 10; c++) {
+        printf("%c ", colunaLetra[c]);
+    }
+    printf("\n");
+
+    return 0;
+}
